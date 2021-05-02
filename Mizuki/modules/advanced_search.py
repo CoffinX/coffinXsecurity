1eece132573761f73e86216aa7d78eaab314271d
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-02 18:55:55 +0530

    Update advanced_search.py

--- Mizuki/modules/advanced_search.py
+++ Mizuki/modules/advanced_search.py
@@ -116,11 +116,13 @@ async def ytsearch(_, message: Message):
         i = 0
         text = ""
         while i < 4:
+            text += f"╔━━━━━━━━⊰✦⊱━━━━━━━━╗\n"
             text += f"Title - {results[i]['title']}\n"
             text += f"Duration - {results[i]['duration']}\n"
             text += f"Views - {results[i]['views']}\n"
             text += f"Channel - {results[i]['channel']}\n"
             text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"╚━━━━━━━━⊰✦⊱━━━━━━━━╝\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
